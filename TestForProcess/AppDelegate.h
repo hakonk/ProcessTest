c1cb672af3a0d0bdb853a2c04954d204ad1ec984
Håkon Knutzen <hakonknutzen@Hakons-MacBook-Pro.local>
2015-03-03 14:15:52 +0100

    Initial Commit

---
+++ TestForProcess/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestForProcess
+//
+//  Created by Håkon Knutzen on 03/03/15.
+//  Copyright (c) 2015 Håkon Knutzen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TestForProcess/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TestForProcess
+//
+//  Created by Håkon Knutzen on 03/03/15.
+//  Copyright (c) 2015 Håkon Knutzen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
